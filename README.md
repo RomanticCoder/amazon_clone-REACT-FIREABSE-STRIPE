39ef9d2dc68c255372e22659f1f074b6f639adf9
lee <letmalikdothat@gmail.com>
2022-05-18 22:14:14 -0400

    6. Setting up Firebase

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 clone coding
 
 https://medium.com/cleverprogrammer/amazon-clone-using-react-the-ultimate-guide-fba2b36f3458
+
+## firebase setup
+firebase login
+firebase init
+
+{
+  "hosting": {
+    "site": "amz-clone-jordan-lee",
+
+    "public": "public",
+    ...
+  }
+}
+
+"site": "amz-clone-jordan-lee",
+
+firebase deploy --only hosting:amz-clone-jordan-lee
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDrE4hr84U40OdU5OP3kZpgfkF_zEfdtU",
+  authDomain: "fir-cfbe0.firebaseapp.com",
+  projectId: "fir-cfbe0",
+  storageBucket: "fir-cfbe0.appspot.com",
+  messagingSenderId: "781558029013",
+  appId: "1:781558029013:web:a1107244339cf5fde2205c"
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
+
+export const authService = getAuth(firebaseApp);
+export const dbService = getFirestore();
+// export const storageService = getStorage();
