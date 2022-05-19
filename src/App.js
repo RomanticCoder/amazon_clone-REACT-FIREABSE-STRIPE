c37a8e5960c7d4e293b353e3083d2c929f582c94
lee <letmalikdothat@gmail.com>
2022-05-18 22:36:52 -0400

    7. Setting up Authentication

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Header from './Header';
+import Login from './Login';
+import { auth } from "./firebase";
+import {onAuthStateChanged} from 'firebase/auth'
 
 function App() {
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+        // dispatch({
+        //   type: "SET_USER",
+        //   user: authUser,
+        // });
+      } else {
+        // the user is logged out
+        // dispatch({
+        //   type: "SET_USER",
+        //   user: null,
+        // });
+      }
+    });
+  }, []);
   return (
     <div className="App">
       <Router>
         <Header/>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
         </Routes>
       </Router>
     </div>
