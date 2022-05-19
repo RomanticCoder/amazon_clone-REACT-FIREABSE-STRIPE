6d1aeb8859a39507e66beb00af4219c57eee44a9
lee <letmalikdothat@gmail.com>
2022-05-18 20:26:31 -0400

    2. Creating the Navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Router>
         <Routes>
           <Route path="/" element={<Home/>}/>
